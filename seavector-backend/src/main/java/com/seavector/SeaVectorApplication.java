a1094cbfe12510f7cea4aff9984395842298bd95
Jesus <jcampoy21@campusuninunez.edu.co>
2026-05-09 00:57:29 -0500

    Version 1.8

---
+++ seavector-backend/src/main/java/com/seavector/SeaVectorApplication.java
@@ -0,0 +1,12 @@
+package com.seavector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaVectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaVectorApplication.class, args);
+    }
+}
